ce29b228ec4936230041af988fd7c6492cfd2121
RX14 <chris@rx14.co.uk>
2016-10-29 14:10:22 +0100

    Disable benchmark interactive mode on travis

--- bench.cr
+++ bench.cr
@@ -1,7 +1,7 @@
 require "./src/rpn"
 require "benchmark"
 
-Benchmark.ips do |b|
+Benchmark.ips(interactive: STDOUT.tty? && !ENV["TRAVIS"]?) do |b|
   b.report("parse infix") { RPN.from_infix("3 + 4 * 2 / (1 - 5)^2^3") }
   b.report("parse rpn") { RPN.from_string("3 4 2 * 1 5 - 2 3^^ / +") }
   b.report("execute") { RPN.execute([15, 1, 4, :"+", 3, :"^", 5, :"*", :"+", 3, :"-"]) }
